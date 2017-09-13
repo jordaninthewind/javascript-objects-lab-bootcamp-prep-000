288d1fcda39753b0b08df3d2921d562c17b5723c
jordaninthewind <jordan.kline@gmail.com>
2017-09-13 16:37:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var recipes = {
 };
 
 function updateObjectWithKeyAndValue(object, key, value) {
-    object = (object[key] = value);
+    return object;
+    object[key] = value;
     return object;
 }
